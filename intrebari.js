69e524762fc01020bfc17e060fd5902a9f5164cb
Simina08Barbu <115221512+Simina08Barbu@users.noreply.github.com>
2023-01-10 22:54:47 +0200

    Create intrebari.js

---
+++ intrebari.js
@@ -0,0 +1,110 @@
+let questions = [{
+        numb: 1,
+        question: "What is the rarest M&M colour ?",
+        answer: "brown",
+        options: [
+            "red",
+            "green",
+            "brown",
+            "yellow"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Who are the stepsisters of Cinderella ?",
+        answer: "Anastasia and Drizella",
+        options: [
+            "Mary and Snow White",
+            "Anastasia and Mirabella",
+            "Mary and Tiana",
+            "Anastasia and Drizella"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Whoever threw that paper you mom's a ....",
+        answer: "h*e",
+        options: [
+            "h*e",
+            "fish",
+            "dog",
+            "block"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Who won the most Ballon d'Or awards?",
+        answer: "Lionel Messi",
+        options: [
+            "Jamal",
+            "Lionel Messi",
+            "Cristiano Ronaldo",
+            "Karim Benzema"
+        ]
+    },
+    {
+        numb: 5,
+        question: "What height is the tallest man in the world ?",
+        answer: "2.51 m",
+        options: [
+            "1.98 m",
+            "2.30 m",
+            "2.51 m",
+            "1.50 m"
+        ]
+    },
+    {
+        numb: 6,
+        question: "What is the longest marathon in the world ? ",
+        answer: "5000 km",
+        options: [
+            "6700 km",
+            "3900 km",
+            "2800 km",
+            "5000 km"
+        ]
+    },
+    {
+        numb: 7,
+        question: "What is the most popular ecuation in the world ?",
+        answer: "E=mc²",
+        options: [
+            "E=mc²",
+            "a²=b²+c²",
+            "F=m*a",
+            "i²=-1"
+        ]
+    },
+    {
+        numb: 8,
+        question: "What was bitcoin's highest price ?",
+        answer: "64.800",
+        options: [
+            "40.000",
+            "67.000",
+            "100.000",
+            "64.800"
+        ]
+    },
+    {
+        numb: 9,
+        question: "What is the most popular longery car brand ?",
+        answer: "Mercedes Benz",
+        options: [
+            "Tesla",
+            "BMW",
+            "Porsche",
+            "Mercedes Benz"
+        ]
+    },
+    {
+        numb: 10,
+        question: "What did Thomas Edison invent ?",
+        answer: "all of them",
+        options: [
+            "Incandescent light bulb",
+            "Gramophone",
+            "all of them",
+            "Phonograph cylinder"
+        ]
+    },
