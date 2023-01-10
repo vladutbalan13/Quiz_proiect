ec1d5c4637bb8b6ac361f780625d0997ce96982a
MariaBejenaru33 <115221544+MariaBejenaru33@users.noreply.github.com>
2023-01-10 22:47:59 +0200

    Update script.js

--- script.js
+++ script.js
@@ -81,3 +81,105 @@ next_btn.onclick = () => {
         showResult();
     }
 }
+function showQuetions(index) {
+    const que_text = document.querySelector(".que_text");
+
+
+    let que_tag = '<span>' + questions[index].numb + ". " + questions[index].question + '</span>';
+    let option_tag = '<div class="option"><span>' + questions[index].options[0] + '</span></div>' +
+        '<div class="option"><span>' + questions[index].options[1] + '</span></div>' +
+        '<div class="option"><span>' + questions[index].options[2] + '</span></div>' +
+        '<div class="option"><span>' + questions[index].options[3] + '</span></div>';
+    que_text.innerHTML = que_tag; 
+    option_list.innerHTML = option_tag; 
+
+    const option = option_list.querySelectorAll(".option");
+
+    
+    for (i = 0; i < option.length; i++) {
+        option[i].setAttribute("onclick", "optionSelected(this)");
+    }
+}
+
+
+
+function optionSelected(answer) {
+    clearInterval(counter); 
+    clearInterval(counterLine);
+    let userAns = answer.textContent; 
+    let correcAns = questions[que_count].answer; 
+    const allOptions = option_list.children.length; 
+
+    if (userAns == correcAns) { 
+        userScore += 1; 
+        answer.classList.add("correct");
+        console.log("Correct Answer");
+        console.log("Your correct answers = " + userScore);
+    } else {
+        answer.classList.add("incorrect"); 
+        console.log("Wrong Answer");
+
+        for (i = 0; i < allOptions; i++) {
+            if (option_list.children[i].textContent == correcAns) {  
+                option_list.children[i].setAttribute("class", "option correct");
+                console.log("Auto selected correct answer.");
+            }
+        }
+    }
+    for (i = 0; i < allOptions; i++) {
+        option_list.children[i].classList.add("disabled");  
+    }
+    next_btn.classList.add("show"); 
+}
+
+function showResult() {
+    info_box.classList.remove("activeInfo"); 
+    quiz_box.classList.remove("activeQuiz"); 
+    result_box.classList.add("activeResult"); 
+    const scoreText = result_box.querySelector(".score_text");
+    if (userScore > 3) { 
+        let scoreTag = '<span>Wow! You got <p>' + userScore + '</p> out of <p>' + questions.length + '</p></span>';
+        scoreText.innerHTML = scoreTag; 
+    } else if (userScore > 1) { 
+        let scoreTag = '<span>Nice! You got <p>' + userScore + '</p> out of <p>' + questions.length + '</p></span>';
+        scoreText.innerHTML = scoreTag;
+    } else { 
+        let scoreTag = '<span>Meh...You only got <p>' + userScore + '</p> out of <p>' + questions.length + '</p></span>';
+        scoreText.innerHTML = scoreTag;
+    }
+}
+
+function startTimer(time) {
+    counter = setInterval(timer, 1000);
+
+    function timer() {
+        timeCount.textContent = time; 
+        time--; 
+        if (time < 9) { 
+            let addZero = timeCount.textContent;
+            timeCount.textContent = "0" + addZero;
+        }
+        if (time < 0) { 
+            clearInterval(counter); 
+            timeText.textContent = "Time Off";
+            const allOptions = option_list.children.length; 
+            let correcAns = questions[que_count].answer; 
+            for (i = 0; i < allOptions; i++) {
+                if (option_list.children[i].textContent == correcAns) { 
+                    option_list.children[i].setAttribute("class", "option correct");
+                    console.log("Time Off: Auto selected correct answer.");
+                }
+            }
+            for (i = 0; i < allOptions; i++) {
+                option_list.children[i].classList.add("disabled"); 
+            }
+            next_btn.classList.add("show"); 
+        }
+    }
+}
+
+
+function queCounter(index) {
+    let totalQueCounTag = '<span><p>' + index + '</p> of <p>' + questions.length + '</p> Questions</span>';
+    bottom_ques_counter.innerHTML = totalQueCounTag; 
+}
